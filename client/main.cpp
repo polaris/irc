27a536ddbd3c46f360366d7cfa23a71b3e6b29d5
Jan Deinhard <jan.deinhard@gmail.com>
2015-08-18 21:52:31 +0200

    adapt main.cpp to previous changes

--- client/main.cpp
+++ client/main.cpp
@@ -1,36 +1,6 @@
+#include "IrcClient.h"
+
 #include <iostream>
-#include <memory>
-#include <string>
-
-#include "Connection.h"
-#include "ConnectionDelegate.h"
-
-class IrcClient
-: public ConnectionDelegate
-, public std::enable_shared_from_this<IrcClient> {
-public:
-    static std::shared_ptr<IrcClient> Create() {
-        return std::shared_ptr<IrcClient>(new IrcClient);
-    }
-
-    void connect(const std::string &address, unsigned short port) {
-        connection = Connection::Create(address, port, shared_from_this());
-    } 
-
-    void onConnect() {
-        connection->write("NICK guest");
-        connection->write("USER guest 0 * :Ronnie Reagan");
-    }
-
-    void onError() {
-    }
-
-private:
-    IrcClient() {
-    }
-
-    std::shared_ptr<Connection> connection;
-};
 
 int main(int, char**) {
     auto client = IrcClient::Create();
