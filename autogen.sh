3626d33a6e417296241ad07dff4e6ec2798b9ea2
Jan Deinhard <jan.deinhard@gmail.com>
2015-08-18 22:05:37 +0200

    add autotools stuff

---
+++ autogen.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+autoreconf --install
+􏰀automake --add-missing --copy >/dev/null 2>&1
---
+++ cleanup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+make clean
+rm -rf autom4te.cache
+rm -f aclocal.m4
+rm -f compile
+rm -f config.h
+rm -f config.h.in
+rm -f config.log
+rm -f config.status
+rm -f configure
+rm -f depcomp
+rm -f install-sh
+rm -f Makefile
+rm -f Makefile.in
+rm -f missing
+rm -f stamp-h1
+rm -f client/Makefile
+rm -f client/Makefile.in
+rm -rf client/.deps
+rm -f server/Makefile
+rm -f server/Makefile.in
+rm -rf server/.deps
